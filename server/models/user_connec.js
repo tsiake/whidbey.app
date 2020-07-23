39ef0e0a54b22676e987f600efc3bfd1ae81f2ef
Zach <zach.noble.smith@gmail.com>
2020-07-23 21:49:04 +0000

    remove unneeded files

--- server/models/user_connec.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/users');
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {});
-
-module.exports = db;
