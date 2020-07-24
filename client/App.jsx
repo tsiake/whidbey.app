21720f362368d05b968ce30483cdc82e41cf5154
Zach <zach.noble.smith@gmail.com>
2020-07-24 01:53:14 +0000

    reformat, remove crud

--- client/App.jsx
+++ client/App.jsx
@@ -10,43 +10,38 @@ import './assets/css/footer.css';
 import './assets/bootstrap/dist/css/bootstrap.min.css';
 
 // components
-// import HomeContainer from './components/home/HomeContainer.jsx';
+import HomeContainer from './components/home/HomeContainer.jsx';
 import NavbarContainer from './components/nav/NavbarContainer.jsx';
 import Footer from './components/nav/Footer.jsx';
 import RegisterContainer from './components/account/RegisterContainer.jsx';
-import Home from './components/home/LP.jsx';
 
 //actions
 
 class App extends React.Component {
 
-	constructor() {
-		super()
-	}
+  constructor() {
+    super()
+  }
 
-	componentDidMount() {
-	}
+  componentDidMount() { }
 
-	render() {
-		return (
-			<div className = "App">
-				<Router>
-					<div className = "app_content">
-						<div className = "main_content">
-							<NavbarContainer />
-							<Switch>
-                                <Route exact path="/" component = { Home } />
-                                <Route exact path="/register" component = { RegisterContainer } />
-                                <Route exact path="/discord"> <Redirect to="https://discord.gg/BukGnYw" /> </Route>
-							</Switch>
-                            <Footer />
-						</div>
-					</div>
-                </Router>
-            </div>
-		);
-	};
-
+  render() {
+    return (
+      <div className = "App">
+        <Router>
+          <div className = "main_content">
+            <NavbarContainer />
+            <Switch>
+              <Route exact path="/" component = { HomeContainer } />
+              <Route exact path="/register" component = { RegisterContainer } />
+              <Route exact path="/discord"> <Redirect to="https://discord.gg/BukGnYw" /> </Route>
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </div>
+    );
+  };
 }
 
 export default App;
